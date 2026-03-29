d55e25b265fd9c2a538bf8e9b9ece0587035e1d9
Anu <anuk041105@gmail.com>
2026-03-29 12:00:07 +0530

    ArrowFunction
    Signed-off-by: Anu <anuk041105@gmail.com>

---
+++ arrowfun.js
@@ -0,0 +1,34 @@
+//Tasks on Functions:
+//1.Create an arrow function to receive two inputs as parameters and return their sum
+const sum = (a,b) => {
+console.log(a + b)
+}
+sum(20,87)
+
+
+//2.Create an arrow function to receive an input as parameter and return its square
+const number = (c) => {
+console.log(c ** 2)
+}
+number(8);
+
+
+//3.Create an arrow function to print “Hello World!”
+const greeting = () => { 
+  console.log("Hello World!")
+}
+greeting()
+
+
+//4.Create an arrow function to receive the name of the user as parameter and print that name along with a message “HI”
+const greeting1 = (name) => {
+console.log(`HI,${name}!`)
+}
+greeting1("Anu")
+
+
+//5.Create an arrow function to receive two inputs as parameters, multiply them and store it in a variable , then print that variable’s value along with the message “Multiplication : “
+ const product = (d,e) => {
+    console.log(d * e)
+ }
+ product(14,14)
